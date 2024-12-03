343e70d831ae21a2fcb9f797bd45a690007711f0
Nehemiah Madahana <nehemiahmadahana230@gmail.com>
2024-12-03 20:32:16 +0300

    First Trial

--- lib/student.py
+++ lib/student.py
@@ -2,7 +2,22 @@
 
 from user import User
 
+import ipdb
+
 class Student(User):
+
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self._knowledge = []
     
-    def learn(self):
-        pass
+    @property
+    def knowledge(self):
+        return self._knowledge
+    
+    @knowledge.setter
+    def knowledge(self, lesson):
+        self._knowledge.append(lesson)
+
+    def learn(self, lesson):
+        self.knowledge = lesson
+
--- lib/user.py
+++ lib/user.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python
 
+import ipdb
+
 class User:
-    pass
+    
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __repr__(self):
+        return f"first name = \"{self.first_name}\", last name = \"{self.last_name}\""
+    
+    # @property
+    # def first_name(self):
+    #     return self._first_name
+    
+    # @first_name.setter
+    # def first_name(self, first_name):
+    #     self._first_name = first_name
+
+    # @property
+    # def last_name(self):
+    #     return self._last_name
+    
+    # @last_name.setter
+    # def last_name(self, last_name):
+    #     self._last_name = last_name
+
